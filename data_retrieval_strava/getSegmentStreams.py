f49bd418d7d56e578df36cc4a1155652defcad14
Lei Yang <lei.yng@gmail.com>
2015-10-16 09:56:55 -0400

    Create getSegmentStreams.py

---
+++ data_retrieval_strava/getSegmentStreams.py
@@ -0,0 +1,14 @@
+# retrieve segment stream
+def getSegmentStreams(segments):
+    ids = []
+    streams = []
+    for seg in segments:
+        sid = int(seg[0])
+        if sid in ids:
+            continue
+        ids.append(sid)
+        stm = client.get_segment_streams(sid,['latlng','altitude','grade_smooth','cadence'])
+        streams.append([seg[0], str(stm['distance'].data).replace(', ','|'),
+                        str(stm['altitude'].data).replace(', ','|'),
+                        str(stm['latlng'].data).replace(', ','|')])
+    return streams
