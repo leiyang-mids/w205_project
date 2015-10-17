08b69f58bae724fce3e56339542ce8b912e048fa
Lei Yang <lei.yng@gmail.com>
2015-10-16 20:03:38 -0400

    Update saveArrayToCSV.py

--- data_retrieval_strava/saveArrayToCSV.py
+++ data_retrieval_strava/saveArrayToCSV.py
@@ -1,5 +1,7 @@
 # save array into CSV file
+import csv
+
 def saveArrayToCSV(info_array, file_name):
     with open(file_name, "a") as f:
         writer = csv.writer(f)
-        writer.writerows(info_array) 
+        writer.writerows(info_array)
