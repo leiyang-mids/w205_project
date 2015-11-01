3508ab249994ea8d908bbd34d64f70d77d023bc1
LEI YANG <lei.yng@gmail.com>
2015-11-01 14:45:17 -0500

    Add Seattle area.

--- data_retrieval_strava/main.py
+++ data_retrieval_strava/main.py
@@ -33,7 +33,7 @@ if (not os.path.isfile(file_activity)):
 #lat1, lon1, lat2, lon2 = 38.790184, -107.567708, 40.719907, -104.376179  # Denver, CO
 #lat1, lon1, lat2, lon2 = 40.572189, -74.877846, 41.894049, -71.993935  # NYC metro, NY
 #lat1, lon1, lat2, lon2 = 36.870949, -122.808137, 39.316351, -118.886018  # Bay Area, CA
-lat1, lon1, lat2, lon2 = 45.125420, -124.497473, 50.568858, -118.825430  # around Seattle, WA 
+lat1, lon1, lat2, lon2 = 45.125420, -124.497473, 50.568858, -118.825430  # around Seattle, WA
 
 # 2. divide the range into a grid for scan
 n = 40
@@ -44,7 +44,7 @@ for i in range(n):
 		grids.append([lat1+i*del_lat, lon1+j*del_lon, lat1+(i+1)*del_lat, lon1+(j+1)*del_lon])
 
 # 3. retrieve and save info in each grid
-for i in range(n*n):
+for i in range(20): #n*n):
 	try:
 		print 'retrieving %s/%s: %s' %(i+1, n*n, str(grids[i]))
 		segments, o_seg = getSegmentsInSquare(client, grids[i])
@@ -52,13 +52,12 @@ for i in range(n*n):
 		if (len(segments) == 0):
 			print 'no segment in the grid!'
 			continue
-		else:
-			print len(segments), 'segments returned.'
+
 		leaderboard, attempts = getLeaderBoardAttempts(o_seg)
 		seg_streams = getSegmentStreams(client, segments)
 		activities = getLeaderBoardActivities(attempts)
 		#act_streams = getActivityStreams(client, attempts) # doesn't work can't authorize, must be activity owner
-
+		print len(segments), 'segments,', len(leaderboard), 'leaderboard,', len(seg_streams), 'streams,', len(activities), 'activities.'
 		saveArrayToCSV(segments, file_segment)
 		saveArrayToCSV(leaderboard, file_leaderboard)
 		saveArrayToCSV(seg_streams, file_streams)
