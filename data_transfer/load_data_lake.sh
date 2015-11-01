20d001f6289b3f5361f12bf5990f58f80259068a
root <root@ip-172-31-0-146.ec2.internal>
2015-11-01 21:13:53 +0000

    add loading scripts

---
+++ data_transfer/load_data_lake.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+echo "# creating local folder to download and store the data"
+mkdir /data/w205project
+cd /data/w205project
+
+echo "# downloading and unzip the data"
+wget https://s3-us-west-2.amazonaws.com/w205.data/Sample_1_20151027.zip
+
+wait
+
+unzip Nqc*
+
+echo "# getting rid of header"
+tail -n +2 "activities.csv" > "activities.csv"
+tail -n +2 "leaderboard.csv" > "leaderboard.csv"
+tail -n +2 "segments.csv" > "segments.csv"
+tail -n +2 "wide_segment_streams.csv" > "wide_segment_streams.csv"
+
+echo "# creating /user/w205/project folder in HDFS ..."
+hdfs dfs -rm -r /user/w205/project
+hdfs dfs -mkdir /user/w205/project
+hdfs dfs -mkdir /user/w205/project/activities_csv
+hdfs dfs -mkdir /user/w205/project/leaderboard_csv
+hdfs dfs -mkdir /user/w205/project/segments_csv
+hdfs dfs -mkdir /user/w205/project/wide_segment_streams_csv
+
+echo "# loading the raw data files into HDFS under /user/w205/project ..."
+hdfs dfs -put activities.csv /user/w205/project/activities_csv/activities.csv
+hdfs dfs -put leaderboard.csv /user/w205/project/leaderboard_csv/leaderboard.csv
+hdfs dfs -put segments.csv /user/w205/project/segments_csv/segments.csv
+hdfs dfs -put wide_segment_streams.csv /user/w205/project/wide_segment_streams_csv/wide_segment_streams.csv
+
+echo "# data loading successfully completed!"
+
+cd ..
+rm /data/w205project -r
+echo "# remove local folder, done!"
