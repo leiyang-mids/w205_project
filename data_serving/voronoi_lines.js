9ff4a5e648def24cc72253065173e5ba8b11c198
LEI YANG <lei.yng@gmail.com>
2015-11-28 23:01:24 -0500

    debugging.

--- data_serving/voronoi_lines.js
+++ data_serving/voronoi_lines.js
@@ -24,7 +24,7 @@ function plot_voronoi(csv_data, price) {
 
   // remove previous one then add
   d3.select('#voronoi').remove();
-  svg = d3.select('#content').append("svg").attr('id', 'voronoi')
+  svg = d3.select('body').append("svg").attr('id', 'voronoi')
    .attr("width", width + margin.left + margin.right)
    .attr("height", height + margin.top + margin.bottom)
    .append("g")
@@ -85,7 +85,7 @@ function plot_voronoi(csv_data, price) {
   focus.append("text").attr("y", -10);
 
   var voronoiGroup = svg.append("g").attr("class", "voronoi");
-
+/*
   console.log(timestamp() + ': start building voronoi mesh ...');
   voronoiGroup.selectAll("path")
       .data(voronoi(d3.nest()
@@ -99,7 +99,7 @@ function plot_voronoi(csv_data, price) {
       .on("mouseover", mouseover)
       .on("mouseout", mouseout);
   console.log(timestamp() + ': finish building voronoi mesh ...');
-/*
+
   d3.select("#show-voronoi")
       .property("disabled", false)
       .on("change", function() { voronoiGroup.classed("voronoi--show", this.checked); });
@@ -118,7 +118,7 @@ function plot_voronoi(csv_data, price) {
     focus.attr("transform", "translate(-100,-100)");
   }
   $("body").css("cursor", "default");
-  console.log(timestamp() + ': finish plotting ...');
+  console.log(timestamp() + ': finish plotting');
 }
 
 function converter(csvData, field) {
@@ -127,7 +127,7 @@ function converter(csvData, field) {
   // for each contract, get one row of the weird data format
   return contracts.map(function(contract) {
     var contract_rows = csvData.filter(function(row) { return row.segment == contract; });
-    var baseline = contract_rows.min(function(d) {return d.altitude;});
+    var baseline = d3.min(contract_rows, function(d) { return d.altitude; });
 	  var contract_data = {name: contract, values: null};
 	  contract_data.values = contract_rows.map(function(row) {
 	    return {
